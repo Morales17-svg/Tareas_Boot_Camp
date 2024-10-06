923cbfe96a40da2160cc3624fdcf370c36b39bca
Morales17 <avimorales58@gmail.com>
2024-10-06 15:29:29 -0300

    Agrego en la carpeta llamada Tarea 3 el primer ejercitario de la semana 3

--- tarea3/tarea1_javascript.js
+++ tarea3/tarea1_javascript.js
@@ -59,7 +59,7 @@ createMessageVariable()
 // 6. Crea una variable llamada `resultado` y asígnale la suma de 2 y 3, luego imprime la variable en consola.
 function createSumVariable() {
     let resultado = 2 + 3;
-    console.log("El resultado de la suma es: ", resultado)
+    console.log("El resultado de la suma es: " + resultado)
 }
 
 createSumVariable()
@@ -67,7 +67,72 @@ createSumVariable()
 // 7. Crea una constante llamada `IS_DISABLED` y asígnale el valor booleano `true`, luego imprime la constante en consola.
 function createDisabledConstant() {
     const IS_DISABLED = true
-    console.log("El valor dentro de la constante IS_DISABLED es: ",IS_DISABLED)
+    console.log("El valor dentro de la constante IS_DISABLED es: " + IS_DISABLED)
 }
 
 createDisabledConstant()
+
+// Null y Undefined
+// 8. Crea una variable con `let` llamada `capacidad` y asígnale un valor `null`, luego imprime la variable en consola.
+function createNullVariable() {
+    let capacidad = null
+    console.log("La variable capacidad tiene un valor: " + capacidad)
+}
+
+createNullVariable()
+
+// 9. Crea una variable con `let` llamada `dinero` y asígnale un valor `undefined`, luego imprime la variable en consola.
+function createUndefinedVariable() {
+    let dinero = undefined
+    console.log("El valor en la variable dinero es: " + dinero)
+}
+
+createUndefinedVariable()
+
+// typeof
+// 10. Escribe un código para ver el tipo de una variable llamada `userName` e imprime el tipo de dato en consola.
+function checkUserNameType() {
+    let userName = "Pablo se fue a correr en un maraton y gano"
+    console.log(typeof userName)
+}
+
+checkUserNameType()
+
+// 11. Asegúrate de que `dogId` es una cadena de texto y luego imprime el tipo de dato en consola.
+function checkDogIdIsString() {
+    let dogId = 17
+
+    if (typeof dogId === "string"){
+        console.log("Es valor dentro de la variable es de tipo string")
+    } else{
+        console.log("El valor dentro de la variable no es un string")
+    }
+}
+
+checkDogIdIsString()
+
+// console.log()
+// 12. Imprime un mensaje en la consola utilizando `console.log()`.
+function logMessage() {
+    console.log("Hola! Me llamo Pablo, este es un mensaje que aparecera en consola")
+}
+
+logMessage()
+
+// 13. Utiliza `console.log()` para imprimir el valor de la variable `edad` que tiene el valor 30.
+function logAge() {
+    let edad = 30;
+    console.log("El valor dentro de la variable edad es: " + edad + " años")
+}
+
+logAge()
+
+// 14. Muestra el mensaje 'La versión de JavaScript es' seguido de la variable `version` con el valor 2024 usando `console.log()`.
+function logVersion() {
+    let version = 2024;
+    console.log("La versión de JavaScript es: " + version)
+}
+
+logVersion()
+
+console.log("Pablo Morales:" + " Espero el asado en Diciembre!")
